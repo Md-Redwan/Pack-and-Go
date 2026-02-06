26e6a0571741d0d3e4b47e4696d2cecd314cc3be
Md-Redwan <redwanhosen4@gmail.com>
2026-02-06 23:18:45 +0600

    Project initialize

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-center font-bold text-4xl'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
