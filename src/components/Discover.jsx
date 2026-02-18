0b89c4899d4de119ed1c1555a083a74cae403b3e
Md-Redwan <redwanhosen4@gmail.com>
2026-02-18 20:46:43 +0600

    Discover Section is complete and responsive

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { IoCheckmark } from "react-icons/io5";
+import { FaUser } from "react-icons/fa6";
+import { ImTrophy } from "react-icons/im";
+import { HiUserGroup } from "react-icons/hi2";
+import { ImLocation2 } from "react-icons/im";
+
 import DiscoverCard from './DiscoverCard';
 
 
 const Discover = () => {
   return (
     <div className='font-poppins py-37.5 bg-[#f4f4f4]'>
-      <div className='container flex justify-between'>
-        <div className='w-[35%]'>
+      <div className='container flex flex-col gap-10 lg:gap-0 lg:flex-row justify-between '>
+        <div className='w-auto lg:w-[35%]'>
             <h3 className='font-semibold text-[40px]'>Go Travel, Discover Remember Us</h3>
             <p className='leading-7.5 text-secondary mt-5'>
                 We are self-service data analytics software that lets 
@@ -25,13 +30,18 @@ const Discover = () => {
                 <IoCheckmark className='text-2xl text-primary'/>
                 We are self-service data software visually  appealing data visualizations.
             </p>
-            <button className='py-4 px-9 bg-primary font-semibold rounded-[17px] mt-10 text-white cursor-pointer'>
+            <div className='flex justify-center lg:justify-start'>
+              <button className='py-4 px-9 bg-primary font-semibold rounded-[17px] mt-10 text-white cursor-pointer'>
                 Discover Place
             </button>
+            </div>
         </div>
-        <div className='w-[50%]'>
-          <div>
-            <DiscoverCard/>
+        <div className='w-auto lg:w-[50%]'>
+          <div className='flex flex-wrap items-center justify-center gap-7.5'>
+            <DiscoverCard cardIcon={<FaUser/>} cardPrice="500k+" cardTitle="Satisfied Clients"/>
+            <DiscoverCard cardIcon={<ImTrophy/>} cardPrice="250+" cardTitle="Active Achieve"/>
+            <DiscoverCard cardIcon={<HiUserGroup/>} cardPrice="15k+" cardTitle="Active Members"/>
+            <DiscoverCard cardIcon={<ImLocation2/>} cardPrice="10k+" cardTitle="Tour Destination"/>
           </div>
         </div>
       </div>
--- src/components/DiscoverCard.jsx
+++ src/components/DiscoverCard.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import { FaUser } from "react-icons/fa6";
-import { ImTrophy } from "react-icons/im";
-import { HiUserGroup } from "react-icons/hi2";
-import { ImLocation2 } from "react-icons/im";
 
 
-const DiscoverCard = () => {
+
+const DiscoverCard = ({cardIcon,cardPrice,cardTitle}) => {
   return (
-    <div className='w-67.5 px-8 py-15.5 hover:bg-primary rounded-[17px] group'>
-      <div className='w-18.5 py-5.5 px-5 hover:bg-white bg-[#e5f2ef] rounded-[14px]'>
-        <FaUser className='text-[34.45px] text-primary'/>
+    <div className='transition ease-in-out w-67.5 px-8 py-15.5 hover:bg-primary rounded-[17px] group bg-white'>
+      <div className='w-18.5 py-5.5 px-5 group-hover:bg-white bg-[#e5f2ef] rounded-[14px] text-[34.45px] text-primary'>
+        {cardIcon}
       </div>
-      <h5 className='hover:text-white text-[40px] font-semibold'>500k</h5>
-      <p className='font-medium text-[25px] hover:text-white'>Satisfied Clients</p>
+      <h5 className='group-hover:text-white text-[40px] font-semibold'>{cardPrice}</h5>
+      <p className='font-medium text-[25px] group-hover:text-white'>{cardTitle}</p>
     </div>
   )
 }
