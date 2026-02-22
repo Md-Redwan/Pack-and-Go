f03d394d0b5283138a801751d93bd1f9dd3eed71
Md-Redwan <redwanhosen4@gmail.com>
2026-02-22 14:29:12 +0600

    why choose us section is complete

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -1,70 +1,130 @@
-import React from 'react'
-import SelectedDestinationImg1 from '../assets/SelectedDestinationImg1.png'
-import SelectedDestinationImg2 from '../assets/SelectedDestinationImg2.png'
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionPanel,
+  AccordionTitle,
+} from "flowbite-react";
+import SelectedDestinationImg1 from "../assets/SelectedDestinationImg1.png";
+import SelectedDestinationImg2 from "../assets/SelectedDestinationImg2.png";
+import SelectedDestinationImg3 from "../assets/SelectedDestinationImg3.png";
 import { IoCheckmarkSharp } from "react-icons/io5";
-
 
 const ChooseUs = () => {
   return (
-    <div className='container pt-37.5 font-poppins'>
-        {/* Selected Destination Part is start */}
-      <div className='flex flex-col-reverse lg:flex-row justify-between items-center'>
-        <div className='w-auto lg:w-[40%]'>
-            <h4 className='font-semibold text-[30px] md:text-[35px] lg:text-[40px] text-center lg:text-left mt-10 lg:mt-0'>
-                We have more than 1000 selected Destination
-            </h4>
-            <p className='text-secondary leading-7.5 mt-5 mb-10 text-center lg:text-left'>
-                You can choose one of many option that we have on our website. Every destination have been 4+ star rated by our user before. You can search by clicking our button on below of the this text man.
-            </p>
-            <div className='flex justify-center lg:justify-start'>
-                <button className='py-4 px-8.75 bg-primary text-white rounded-[17px] font-semibold cursor-pointer'>
-                    Search Destination
-                </button>
-            </div>
+    <div className="container pt-37.5 font-poppins">
+      {/* Selected Destination Part is start */}
+      <div className="flex flex-col-reverse lg:flex-row justify-between items-center">
+        <div className="w-auto lg:w-[40%]">
+          <h4 className="font-semibold text-[30px] md:text-[35px] lg:text-[40px] text-center lg:text-left mt-10 lg:mt-0">
+            We have more than 1000 selected Destination
+          </h4>
+          <p className="text-secondary leading-7.5 mt-5 mb-10 text-center lg:text-left">
+            You can choose one of many option that we have on our website. Every
+            destination have been 4+ star rated by our user before. You can
+            search by clicking our button on below of the this text man.
+          </p>
+          <div className="flex justify-center lg:justify-start">
+            <button className="py-4 px-8.75 bg-primary text-white rounded-[17px] font-semibold cursor-pointer">
+              Search Destination
+            </button>
+          </div>
         </div>
-        <div className='w-auto lg:w-[50%] lg:flex lg:justify-end'>
-            <img src={SelectedDestinationImg1} alt="" />
+        <div className="w-auto lg:w-[50%] lg:flex lg:justify-end">
+          <img src={SelectedDestinationImg1} alt="" />
         </div>
       </div>
-        {/* Selected Destination Part is end */}
+      {/* Selected Destination Part is end */}
 
-        {/* Promo every month part is start */}
-        <div className='container flex-col gap-10 lg:gap-0 lg:flex-row flex lg:justify-between lg:items-center pt-22 lg:pt-17.5'>
-            <div className='w-auto lg:w-[50%]'>
-                <img src={SelectedDestinationImg2} alt="" />
-            </div>
-            <div className='w-auto lg:w-[50%] flex flex-col items-end text-start'>
-                <h2 className='font-semibold lg:w-130 w-auto text-[30px] md:text-[35px] lg:text-[40px] text-center lg:text-start'>Always give you promo on every Month / Event</h2>
-                <p className='lg:w-130 w-auto leading-7.5 text-secondary my-5 text-center lg:text-start'>
-                    We make sure give you a lot of promo every motnh or event based on calendar. If you want, just Subscribe on Newsletter.
-                </p>
-                <div className='lg:w-130 w-auto gap-x-7 gap-y-3.75 text-secondary flex flex-wrap justify-center lg:justify-normal'>
-                    <p className='flex items-center gap-2.5 leading-7.5'>
-                        <IoCheckmarkSharp className='text-primary'/>
-                        25% for New Members
-                    </p>
-                    <p className='flex items-center gap-2.5 leading-7.5'>
-                        <IoCheckmarkSharp className='text-primary'/>
-                        Cancelled refund 100%
-                    </p>
-                    <p className='flex items-center gap-2.5 leading-7.5'>
-                        <IoCheckmarkSharp className='text-primary'/>
-                        Up 50% on your birthday
-                    </p>
-                    <p className='flex items-center gap-2.5 leading-7.5'>
-                        <IoCheckmarkSharp className='text-primary'/>
-                        Reward for 1000% Points
-                    </p>
-                    <p className='flex items-center gap-2.5 leading-7.5'>
-                        <IoCheckmarkSharp className='text-primary'/>
-                        Cashback 10% via OVO
-                    </p>
-                </div>
-            </div>
+      {/* Promo every month part is start */}
+      <div className="container flex-col gap-10 lg:gap-0 lg:flex-row flex lg:justify-between lg:items-center pt-22 lg:pt-17.5">
+        <div className="w-auto lg:w-[50%]">
+          <img src={SelectedDestinationImg2} alt="" />
         </div>
-        {/* Promo every month part is end */}
-    </div>
-  )
-}
+        <div className="w-auto lg:w-[50%] flex flex-col items-end text-start">
+          <h2 className="font-semibold lg:w-130 w-auto text-[30px] md:text-[35px] lg:text-[40px] text-center lg:text-start">
+            Always give you promo on every Month / Event
+          </h2>
+          <p className="lg:w-130 w-auto leading-7.5 text-secondary my-5 text-center lg:text-start">
+            We make sure give you a lot of promo every motnh or event based on
+            calendar. If you want, just Subscribe on Newsletter.
+          </p>
+          <div className="lg:w-130 w-auto gap-x-7 gap-y-3.75 text-secondary flex flex-wrap justify-center lg:justify-normal">
+            <p className="flex items-center gap-2.5 leading-7.5">
+              <IoCheckmarkSharp className="text-primary" />
+              25% for New Members
+            </p>
+            <p className="flex items-center gap-2.5 leading-7.5">
+              <IoCheckmarkSharp className="text-primary" />
+              Cancelled refund 100%
+            </p>
+            <p className="flex items-center gap-2.5 leading-7.5">
+              <IoCheckmarkSharp className="text-primary" />
+              Up 50% on your birthday
+            </p>
+            <p className="flex items-center gap-2.5 leading-7.5">
+              <IoCheckmarkSharp className="text-primary" />
+              Reward for 1000% Points
+            </p>
+            <p className="flex items-center gap-2.5 leading-7.5">
+              <IoCheckmarkSharp className="text-primary" />
+              Cashback 10% via OVO
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Promo every month part is end */}
 
-export default ChooseUs
+      {/* Money Back Guarantee Part is start */}
+      <div className="flex flex-col-reverse lg:flex-row justify-between items-center lg:items-start gap-10 pt-22 lg:pt-17.5">
+        <div className="w-auto lg:w-[40%]">
+          <h2 className="font-semibold text-[30px] md:text-[35px] lg:text-[40px] text-center lg:text-start">
+            100% Money back Guarantee If you Cancel
+          </h2>
+          <p className="text-secondary leading-7.5 mt-5 mb-10 text-center lg:text-left">
+            We Promises in this case. We still want to refund your money when
+            you cancel order.
+          </p>
+
+          {/* According is start */}
+          <Accordion collapseAll>
+            <AccordionPanel>
+              <AccordionTitle>Friendly, Quick, Easy</AccordionTitle>
+              <AccordionContent>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                  Business messaging that feels just like the messenger apps you and your customers use every day.
+                </p>
+                
+              </AccordionContent>
+            </AccordionPanel>
+            <AccordionPanel>
+              <AccordionTitle>Fast Response Answer </AccordionTitle>
+              <AccordionContent>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                  Business messaging that feels just like the messenger apps you and your customers use every day.
+                </p>
+              </AccordionContent>
+            </AccordionPanel>
+            <AccordionPanel>
+              <AccordionTitle>
+                Money Back Guarantee
+              </AccordionTitle>
+              <AccordionContent>
+                <p className="mb-2 text-gray-500 dark:text-gray-400">
+                  Business messaging that feels just like the messenger apps you and your customers use every day.
+                </p>
+              </AccordionContent>
+            </AccordionPanel>
+          </Accordion>
+          {/* According is end */}
+        </div>
+        <div className="w-auto lg:w-[40%]">
+          <img src={SelectedDestinationImg3} alt="" />
+        </div>
+      </div>
+      {/* Money Back Guarantee Part is end */}
+    </div>
+  );
+};
+
+export default ChooseUs;
